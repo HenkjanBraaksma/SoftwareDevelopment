fed7963f3bde313d943d80d310b51fc818595282
Unknown <ccs_fan@hotmail.com>
2018-03-27 15:45:10 +0200

    Did all sorts of cool shit
    Made a spawner. Made cars brake and accelerate subtly. Made cars stand still at a safe distance from other cars.

--- TCP IP Test/Assets/Scripts/Car Scripts/CarBehavior.cs
+++ TCP IP Test/Assets/Scripts/Car Scripts/CarBehavior.cs
@@ -4,42 +4,85 @@ using UnityEngine;
 
 public class CarBehavior : MonoBehaviour {
 
-    public float topSpeed = 500;
-    public float topReverse = -100;
-
-    public float acceleration = 1.1f;
-    public float reverseAccelerate = 1.05f;
-
-    private Rigidbody rb;
-    private float currentSpeed = 0;
+    private BoxCollider ownCollider;
+    private float currentSpeed = 45;
+    private bool roadOpen = true;
+    private bool noCar = true;
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
+        ownCollider = GetComponent<BoxCollider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	}
-
-    void FixedUpdate()
-    {
-        if (Input.GetButton("Forward"))
-            Accelerate();
+        Debug.DrawRay(transform.position, transform.forward * 12, Color.red, Time.deltaTime, false);
+        CheckCars();
+        if (roadOpen && noCar)
+            Move();
+        else
+            Brake();
     }
 
-    void Accelerate()
+    void Move()
     {
-        if (currentSpeed < 1)
-            currentSpeed = 1;
+        Vector3 forward = new Vector3(0, 0, currentSpeed);
+        transform.Translate(forward * Time.deltaTime);
 
-        rb.AddRelativeForce(Vector3.forward * currentSpeed);
-
-        if (currentSpeed < topSpeed)
-            currentSpeed *= acceleration;
+        if (!(currentSpeed >= 45))
+        {
+            currentSpeed += Random.Range(1, 7);
+        }
+        if (currentSpeed > 45)
+        {
+            currentSpeed = 45;
+        }
     }
 
-    void Reverse()
+    void Brake()
     {
+        if (currentSpeed > 0)
+        {
+            currentSpeed -= 5;
+            if (currentSpeed < 2)
+                currentSpeed = 0;
+
+            Vector3 forward = new Vector3(0, 0, currentSpeed);
+            transform.Translate(forward * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "StopLine")
+        {
+            Debug.Log("Nice");
+            if (other.gameObject.GetComponentInParent<TrafficLightBehaviour>().lightStatus == "red")
+                roadOpen = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name == "StopLine" && !roadOpen)
+        {
+            if (other.gameObject.GetComponentInParent<TrafficLightBehaviour>().lightStatus == "green")
+                roadOpen = true;
+        }
+    }
+
+    private void CheckCars()
+    {
+        RaycastHit hitInfo;
+        Ray lookAhead = new Ray(transform.position, transform.forward);
+        if (noCar && Physics.Raycast(lookAhead, out hitInfo, 19f))
+        {
+            if (hitInfo.collider.tag == "Car" && hitInfo.collider != ownCollider)
+                noCar = false;
+        }
+        else if (!noCar && Physics.Raycast(lookAhead, out hitInfo, 28f))
+            noCar = false;
+        else
+            noCar = true;
     }
 }
---
+++ TCP IP Test/Assets/Scripts/Car Scripts/SpawnerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+
+    GameObject RedCar;
+    GameObject YellowCar;
+    GameObject PoliceCar;
+    GameObject YellowBus;
+
+	// Use this for initialization
+	void Start () {
+        RedCar = GameObject.Find("RedCar");
+        InvokeRepeating("SpawnCar", 5f, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnCar()
+    {
+        Instantiate(RedCar, new Vector3(603.7f, 3.72f, 371.3f),Quaternion.Euler(0, 270, 0));
+    }
+}
