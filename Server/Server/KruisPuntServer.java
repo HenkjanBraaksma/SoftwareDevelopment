c641df4fa261364958bd6e347ca101200822d4a5
dennyo <bijl1507@student.nhl.nl>
2018-04-05 16:25:11 +0200

    keep connection open and check message type

--- Server/Server/KruisPuntServer.java
+++ Server/Server/KruisPuntServer.java
@@ -1,3 +1,4 @@
+
 import java.net.*;
 import java.io.*;
 import org.json.simple.JSONArray;
@@ -7,89 +8,108 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 class TrafficLight implements JSONAware {
-  private String id;
-  private String lightStatus;
 
-  public TrafficLight(String id, String lightStatus) {
-    this.id = id;
-    this.lightStatus = lightStatus;
-  }
+    private String id;
+    private String lightStatus;
 
-  public String toJSONString() {
-    JSONObject obj = new JSONObject();
-    obj.put("id", id);
-    obj.put("lightStatus", lightStatus);
-    return obj.toString();
-  }
+    public TrafficLight(String id, String lightStatus) {
+        this.id = id;
+        this.lightStatus = lightStatus;
+    }
+
+    public String toJSONString() {
+        JSONObject obj = new JSONObject();
+        obj.put("id", id);
+        obj.put("lightStatus", lightStatus);
+        return obj.toString();
+    }
 }
 
 public class KruisPuntServer {
-   private ServerSocket serverSocket;
-   private Socket server;
-   
-   public KruisPuntServer(int port) throws IOException {
-      serverSocket = new ServerSocket(port);
-      System.out.println("Listening on port " + serverSocket.getLocalPort() + "...");
-   }
 
-   public void run() {
-      while(true) {
-         try {            
+    private ServerSocket serverSocket;
+    private Socket server;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private JSONParser parser;
+
+    public KruisPuntServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("Listening on port " + serverSocket.getLocalPort() + "...");
+    }
+
+    public void run() {
+        try {
             server = serverSocket.accept();
             System.out.println("Connected to client on " + server.getRemoteSocketAddress());
-            
-            JSONParser parser = new JSONParser();
-            InputStreamReader inStream = new InputStreamReader(server.getInputStream());
-            BufferedReader in = new BufferedReader(inStream);
-            String line = in.readLine();
-            Object parsedLine = parser.parse(line);
-            
-            JSONObject receivedObj = (JSONObject)parsedLine;
-            System.out.println("------RECEIVED DATA------");
-            System.out.println("type: " + receivedObj.get("type"));
-            System.out.println("triggered: " + receivedObj.get("triggered"));
-            System.out.println("id: " + receivedObj.get("id"));
-            System.out.println("-------------------------\n");       
-            
-            JSONObject sendObj = new JSONObject();
-            sendObj.put("type", "TrafficLightData");
-            JSONArray list = new JSONArray();
-            list.add(new TrafficLight("1.1", "green"));
-            list.add(new TrafficLight("1.2", "red"));
-            sendObj.put("trafficLights", list);
 
-            System.out.println("SENT DATA: " + sendObj);
+            parser = new JSONParser();
+            InputStreamReader inStream = new InputStreamReader(server.getInputStream());
+            in = new BufferedReader(inStream);
 
             OutputStreamWriter outStream = new OutputStreamWriter(server.getOutputStream());
-            BufferedWriter out = new BufferedWriter(outStream);
-            out.write(sendObj + "\n");
-            out.flush();
-
-            inStream.close();
-            outStream.close();
-         } catch (IOException e) {
+            out = new BufferedWriter(outStream);
+        } catch (IOException e) {
             e.printStackTrace();
-            break;
-         }
-         catch (ParseException pe) {
-             System.out.println("position: " + pe.getPosition());
-             System.out.println(pe);
-             break;
-         }
-      }
-   }
-   
-  public static void main(String [] args) {
-	  if(args.length != 1) {
-		  System.out.println("Usage: java KruisPuntServer <portnumber>");
-		  System.exit(1);
-	  }
-    int port = Integer.parseInt(args[0]);
-    try {
-      KruisPuntServer kpServer = new KruisPuntServer(port);
-      kpServer.run();
-    } catch (IOException e) {
-        System.out.println(e);
+        }
+        while (true) {
+            try {
+                String line = in.readLine();
+                Object parsedLine = parser.parse(line);
+
+                JSONObject receivedObj = (JSONObject) parsedLine;
+                JSONObject sendObj = new JSONObject();
+
+                String type = (String)receivedObj.get("type");
+                System.out.println("type: " + type);
+
+                if("BridgeStatusData".equals(type)) {
+                    System.out.println("opened: " + receivedObj.get("opened"));
+                    sendObj.put("type", "BridgeData");
+                    sendObj.put("bridgeOpen", true);
+                }
+                else if("TimeScaleData".equals(type)) {
+                    System.out.println("scale: " + receivedObj.get("scale"));
+                    sendObj.put("type", "TImeScaleVerifyData");
+                    sendObj.put("status", true);
+                }
+                else if("PrimaryTrigger".equals(type) || "SecondaryTrigger".equals(type)) {
+                    System.out.println("triggered: " + receivedObj.get("triggered"));
+                    System.out.println("id: " + receivedObj.get("id"));
+                    sendObj.put("type", "TrafficLightData");
+                    JSONArray list = new JSONArray();
+                    list.add(new TrafficLight("1.1", "red"));
+                    list.add(new TrafficLight("1.2", "green"));
+                    sendObj.put("trafficLights", list);
+                }
+                System.out.println("SENT DATA: " + sendObj);
+
+                out.write(sendObj + "\n");
+                out.flush();
+//                inStream.close();
+//                outStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            } catch (ParseException pe) {
+                System.out.println("position: " + pe.getPosition());
+                System.out.println(pe);
+                break;
+            }
+        }
     }
-  }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java KruisPuntServer <portnumber>");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+        try {
+            KruisPuntServer kpServer = new KruisPuntServer(port);
+            kpServer.run();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
