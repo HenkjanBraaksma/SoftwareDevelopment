d010174c4ada64685bdd3eceb05c031176e5a70e
Unknown <ccs_fan@hotmail.com>
2018-05-18 10:06:30 +0200

    Fixed pedestrian routes (hopefully)

--- TCP IP Test/Assets/Scripts/Car Scripts/SpawnerScript.cs
+++ TCP IP Test/Assets/Scripts/Car Scripts/SpawnerScript.cs
@@ -62,60 +62,60 @@ public class SpawnerScript : MonoBehaviour {
         Bike = Resources.Load("Prefabs/Bicycle") as GameObject;
 
 
-        ////Cars
-        //spawns.Add(new SpawnerDetails("1.4", "CAR", 158f, -6.6f, Quaternion.Euler(0, 20, 0)));
-        //spawns.Add(new SpawnerDetails("1.5", "CAR", 158f, -6.6f, Quaternion.Euler(0, 20, 0)));
-        //spawns.Add(new SpawnerDetails("1.6", "CAR", 158f, -6.6f, Quaternion.Euler(0, 20, 0)));
-        //spawns.Add(new SpawnerDetails("1.7", "CAR", -1.1f, 316.9f, Quaternion.Euler(0, 90.7f, 0)));
-        //spawns.Add(new SpawnerDetails("1.8", "CAR", -0.7f, 328.9f, Quaternion.Euler(0, 90.7f, 0)));
-        //spawns.Add(new SpawnerDetails("1.8", "CAR", -0.7f, 342.4f, Quaternion.Euler(0, 90.7f, 0)));
-        //spawns.Add(new SpawnerDetails("1.9", "CAR", -1.1f, 354f, Quaternion.Euler(0, 90.7f, 0)));
-        //spawns.Add(new SpawnerDetails("1.10", "CAR", 163.8f, 517.3f, Quaternion.Euler(0, 179.5f, 0)));
-        //spawns.Add(new SpawnerDetails("1.11", "CAR", 179f, 517.4f, Quaternion.Euler(0, 179.5f, 0)));
-        //spawns.Add(new SpawnerDetails("1.12", "CAR", 193f, 517.5f, Quaternion.Euler(0, 179.5f, 0)));
-        //spawns.Add(new SpawnerDetails("1.13", "CAR", 2016.7f, 193.1f, Quaternion.Euler(0, 281, 0)));
+        //Cars
+        spawns.Add(new SpawnerDetails("1.4", "CAR", 158f, -6.6f, Quaternion.Euler(0, 20, 0)));
+        spawns.Add(new SpawnerDetails("1.5", "CAR", 158f, -6.6f, Quaternion.Euler(0, 20, 0)));
+        spawns.Add(new SpawnerDetails("1.6", "CAR", 158f, -6.6f, Quaternion.Euler(0, 20, 0)));
+        spawns.Add(new SpawnerDetails("1.7", "CAR", -1.1f, 316.9f, Quaternion.Euler(0, 90.7f, 0)));
+        spawns.Add(new SpawnerDetails("1.8", "CAR", -0.7f, 328.9f, Quaternion.Euler(0, 90.7f, 0)));
+        spawns.Add(new SpawnerDetails("1.8", "CAR", -0.7f, 342.4f, Quaternion.Euler(0, 90.7f, 0)));
+        spawns.Add(new SpawnerDetails("1.9", "CAR", -1.1f, 354f, Quaternion.Euler(0, 90.7f, 0)));
+        spawns.Add(new SpawnerDetails("1.10", "CAR", 163.8f, 517.3f, Quaternion.Euler(0, 179.5f, 0)));
+        spawns.Add(new SpawnerDetails("1.11", "CAR", 179f, 517.4f, Quaternion.Euler(0, 179.5f, 0)));
+        spawns.Add(new SpawnerDetails("1.12", "CAR", 193f, 517.5f, Quaternion.Euler(0, 179.5f, 0)));
+        spawns.Add(new SpawnerDetails("1.13", "CAR", 2016.7f, 193.1f, Quaternion.Euler(0, 281, 0)));
 
-        ////Bikes
-        //spawns.Add(new SpawnerDetails("A2A1", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
-        //spawns.Add(new SpawnerDetails("A2B1", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
-        //spawns.Add(new SpawnerDetails("A2C1", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
-        //spawns.Add(new SpawnerDetails("A2D1", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
-        //spawns.Add(new SpawnerDetails("A2D2", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
+        //Bikes
+        spawns.Add(new SpawnerDetails("A2A1", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
+        spawns.Add(new SpawnerDetails("A2B1", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
+        spawns.Add(new SpawnerDetails("A2C1", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
+        spawns.Add(new SpawnerDetails("A2D1", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
+        spawns.Add(new SpawnerDetails("A2D2", "BIKE", 7.5f, 293.7f, Quaternion.Euler(0, 91, 0)));
 
-        //spawns.Add(new SpawnerDetails("B2A1", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
-        //spawns.Add(new SpawnerDetails("B2B1", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
-        //spawns.Add(new SpawnerDetails("B2C1", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
-        //spawns.Add(new SpawnerDetails("B2D1", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
-        //spawns.Add(new SpawnerDetails("B2D2", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
+        spawns.Add(new SpawnerDetails("B2A1", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
+        spawns.Add(new SpawnerDetails("B2B1", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
+        spawns.Add(new SpawnerDetails("B2C1", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
+        spawns.Add(new SpawnerDetails("B2D1", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
+        spawns.Add(new SpawnerDetails("B2D2", "BIKE", 151.3f, 507.3f, Quaternion.Euler(0, 180, 0)));
 
-        //spawns.Add(new SpawnerDetails("C2A1", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
-        //spawns.Add(new SpawnerDetails("C2B1", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
-        //spawns.Add(new SpawnerDetails("C2C1", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
-        //spawns.Add(new SpawnerDetails("C2D1", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
-        //spawns.Add(new SpawnerDetails("C2D2", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
+        spawns.Add(new SpawnerDetails("C2A1", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
+        spawns.Add(new SpawnerDetails("C2B1", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
+        spawns.Add(new SpawnerDetails("C2C1", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
+        spawns.Add(new SpawnerDetails("C2D1", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
+        spawns.Add(new SpawnerDetails("C2D2", "BIKE", 2041.8f, 212.69f, Quaternion.Euler(0, 281, 0)));
 
-        //spawns.Add(new SpawnerDetails("D2A1", "BIKE", 222.2f, 7.7f, Quaternion.Euler(0, 27.5f, 0)));
-        //spawns.Add(new SpawnerDetails("D2B1", "BIKE", 222.2f, 7.7f, Quaternion.Euler(0, 27.5f, 0)));
-        //spawns.Add(new SpawnerDetails("D2C1", "BIKE", 222.2f, 7.7f, Quaternion.Euler(0, 27.5f, 0)));
-        //spawns.Add(new SpawnerDetails("D2D1", "BIKE", 222.2f, 7.7f, Quaternion.Euler(0, 27.5f, 0)));
+        spawns.Add(new SpawnerDetails("D2A1", "BIKE", 222.2f, 7.7f, Quaternion.Euler(0, 27.5f, 0)));
+        spawns.Add(new SpawnerDetails("D2B1", "BIKE", 222.2f, 7.7f, Quaternion.Euler(0, 27.5f, 0)));
+        spawns.Add(new SpawnerDetails("D2C1", "BIKE", 222.2f, 7.7f, Quaternion.Euler(0, 27.5f, 0)));
+        spawns.Add(new SpawnerDetails("D2D1", "BIKE", 222.2f, 7.7f, Quaternion.Euler(0, 27.5f, 0)));
 
-        ////Humans
-        //spawns.Add(new SpawnerDetails("HA1A2", "HUMAN", 1.7f, 427.7f, Quaternion.Euler(0, 90, 0)));
+        //Humans
+        spawns.Add(new SpawnerDetails("HA1A2", "HUMAN", 1.7f, 427.7f, Quaternion.Euler(0, 90, 0)));
 
-        //spawns.Add(new SpawnerDetails("HA2A1", "HUMAN", 1.7f, 284f, Quaternion.Euler(0, 90, 0)));
-        //spawns.Add(new SpawnerDetails("HA2B1", "HUMAN", 1.7f, 284f, Quaternion.Euler(0, 90, 0)));
-        //spawns.Add(new SpawnerDetails("HA2D2", "HUMAN", 1.7f, 284f, Quaternion.Euler(0, 90, 0)));
+        spawns.Add(new SpawnerDetails("HA2A1", "HUMAN", 1.7f, 281f, Quaternion.Euler(0, 90, 0)));
+        spawns.Add(new SpawnerDetails("HA2B1", "HUMAN", 1.7f, 284f, Quaternion.Euler(0, 90, 0)));
+        spawns.Add(new SpawnerDetails("HA2D2", "HUMAN", 1.7f, 284f, Quaternion.Euler(0, 90, 0)));
 
-        //spawns.Add(new SpawnerDetails("HB1A1", "HUMAN", 260.4f, 509.6f, Quaternion.Euler(0, 165, 0)));
+        spawns.Add(new SpawnerDetails("HB1A1", "HUMAN", 260.4f, 509.6f, Quaternion.Euler(0, 165, 0)));
         spawns.Add(new SpawnerDetails("HB1A2", "HUMAN", 260.4f, 509.6f, Quaternion.Euler(0, 165, 0)));
-        //spawns.Add(new SpawnerDetails("HB1D1", "HUMAN", 260.4f, 509.6f, Quaternion.Euler(0, 165, 0)));
+        spawns.Add(new SpawnerDetails("HB1D2", "HUMAN", 260.4f, 509.6f, Quaternion.Euler(0, 165, 0)));
 
-        //spawns.Add(new SpawnerDetails("HB2B1", "HUMAN", 140.6f, 508.6f, Quaternion.Euler(0, 195, 0)));
+        spawns.Add(new SpawnerDetails("HB2B1", "HUMAN", 140.6f, 508.6f, Quaternion.Euler(0, 195, 0)));
 
 
         //Boats
-        //spawns.Add(new SpawnerDetails("4.2", "BOAT", 1989f, 486f, Quaternion.Euler(0, 215, 0)));
-        //spawns.Add(new SpawnerDetails("4.1", "BOAT", 1709f, 21f, Quaternion.Euler(0, 35, 0)));
+        spawns.Add(new SpawnerDetails("4.2", "BOAT", 1989f, 486f, Quaternion.Euler(0, 215, 0)));
+        spawns.Add(new SpawnerDetails("4.1", "BOAT", 1709f, 21f, Quaternion.Euler(0, 35, 0)));
 
         if (isOn)
             InvokeRepeating("SpawnCar", 5f, 2f);
